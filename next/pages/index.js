c971a5e32c9a80446a800a13db96b2d853cc2eca
Thomas Guillet <guillet.thomas@gmail.com>
2019-11-16 11:00:38 +0100

    Ajoute les résultats de sondage

--- next/pages/index.js
+++ next/pages/index.js
@@ -5,6 +5,38 @@ import { scaleOrdinal } from 'd3-scale'
 import { ResponsiveBar } from '@nivo/bar'
 import 'iframe-resizer'
 
+
+const surveyLabels = {
+    asked: {
+        legend: "Au moins une prestation de la simulation a fait l'objet d'une demande réussie (c'est à dire que l'usager a été au bout de la démarche)",
+        short: 'Demande réussie',
+        single: 'D. réussie'
+    },
+    failed: {
+        legend: "Au moins une prestation de la simulation a fait l'objet d'une demande mais l'usager <b>N'A PAS</b> réussi à aller au bout de la démarche",
+        short: 'Demande échouée',
+        single: 'D. échouée'
+    },
+    nothing: {
+        legend: "Aucune prestation n'a fait l'objet d'une demande alors qu'elles ne sont pas perçues",
+        short: 'Aucune demande',
+        single: 'Rien fait'
+    },
+    already: {
+        legend:"Toutes les prestations affichées étaient déjà perçues",
+        short: 'Déjà perçues',
+        single: 'Déjà perçue'
+    }
+}
+
+const globalSurveyResults = [
+    { category: 'asked', value: 305 },
+    { category: 'failed', value: 110 },
+    { category: 'nothing', value: 133 },
+    { category: 'already', value: 22 },
+]
+const surveyIds = Object.keys(surveyLabels)
+
 const catMapping = {
     show: { cat: 'Affiché' },
 
@@ -33,7 +65,13 @@ const cats = [
     'Incompris',
     'Expliqué'
 ]
-const colors = scaleOrdinal(categoricalColorSchemes.category10)
+const actionColors = scaleOrdinal(categoricalColorSchemes.category10)
+const surveyColors = scaleOrdinal([
+    '#2ca02c',
+    '#ff7f0e',
+    '#d62728',
+    '#7f7f7f',
+]).domain(Object.keys(surveyLabels))
 
 function apply(prop, base, shouldShow) {
     let result = base.subtable.reduce((accum, table) => {
@@ -72,21 +110,31 @@ const periods = {
 // no chart will be rendered.
 // website examples showcase many properties,
 // you'll often use just a few of them.
-const MyResponsiveBar = ({ data /* see data tab */ }) => (
+const ActionResponsiveBar = ({ data /* see data tab */ }) => (
     <ResponsiveBar
         data={data}
         keys={Object.keys(catMapping)}
         indexBy="category"
         margin={{ top: 15, right: 10, bottom: 50, left: 60 }}
         padding={0.3}
-        colors={({ id }) => colors(id)}
+        colors={({ id }) => actionColors(id)}
         borderColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
         animate={false}
     />
 )
 
 function Home() {
+    const [survey, setSurvey] = useState({
+        summary: [],
+        details: {
+            data:[],
+            maxPercentage: 100,
+        }
+    });
     const [benefits, setBenefits] = useState([]);
+    const [showActions, setShowActions] = useState(true);
+    const [showSurveyDetails, setShowSurveyDetails] = useState(true);
+    const [openfiscaVariables, setOpenfiscaVariables] = useState({});
     const [period, setPeriod] = useState('day');
     const [source, setSource] = useState('nb_visits');
     const [show, setShow] = useState(cats.reduce((accum, c) => {
@@ -104,8 +152,69 @@ function Home() {
         }
     }
 
+    async function fetchSurveyData(period) {
+        try {
+            const res = await fetch('https://mes-aides.gouv.fr/documents/stats.json')
+            const json = await res.json()
+
+            const summary = surveyIds.map(id => {
+                return {
+                    key: id,
+                    category: surveyLabels[id].short,
+                    value: json.survey.summary[id]
+                }
+            })
+
+            const details = json.survey.details.map(d => {
+                const data =  surveyIds.map(id => {
+                    return {
+                        id: d.id,
+                        key: id,
+                        category: surveyLabels[id].single,
+                        value: d[id],
+                        percentage: Math.round(d[id]/d.total*1000)/10 || 0
+                    };
+                });
+
+                return {
+                    id: d.id,
+                    data: data,
+                    total: d.total
+                }
+            })
+            const maxPercentage = Math.min(100, 1.1 *Math.max(...details.map(d => d.total <= 10 ?  0 :  Math.max(...d.data.map(p => p.percentage )))))
+
+            let survey = {
+                summary,
+                details: {
+                    data: details,
+                    maxPercentage,
+                },
+                count: json.survey.summary.total
+            }
+            setSurvey(survey)
+        } catch {
+            setBenefits([])
+        }
+    }
+
+    async function fetchOpenfiscaVariables() {
+        try {
+            const res = await fetch('https://mes-aides.gouv.fr/api/benefits')
+            const json = await res.json()
+            setOpenfiscaVariables(json.reduce((a,v) => {
+                a[v.id] = v
+                return a
+            }, {}))
+        } catch {
+            setOpenfiscaVariables({})
+        }
+    }
+
     useEffect(() => {
       fetchData(period)
+      fetchSurveyData()
+      fetchOpenfiscaVariables()
     }, [])
 
     const handlePeriodChange = useCallback(e => {
@@ -121,6 +230,10 @@ function Home() {
     return (
         <div>
         <style jsx>{`
+            div {
+                font-family: sans;
+            }
+
             .chart {
                 height: 300px;
             }
@@ -128,8 +241,13 @@ function Home() {
                 display: flex;
                 flex-wrap: wrap;
             }
+
+            .doubleCell {
+                max-width: 600px;
+            }
+
             .cell {
-                width: 300px;
+                width: 345px;
                 display: flex;
                 flex-direction: column;
                 justify-content: space-between;
@@ -137,75 +255,137 @@ function Home() {
             h3 {
                 margin: 0;
             }
-          `}</style>
-            <p>
-                Les graphiques suivants représentent les taux de conversion sur la page de présentation de résultats sur le simulateur.
-                Différents évènements sont capturés pour mieux évaluer l'impact du simulateur sur le non-recours aux dispositifs présentés aux usagers.
-            </p>
-            <div className="list">
-                <div className="cell">
-                    <div><label>
-                        Période de référence
-                        <select onChange={handlePeriodChange} defaultValue={period}>
-                        {
-                            Object.keys(periods).map(k => {
-                                return <option key={k} value={k}>{periods[k]}</option>
-                            })
-                        }
-                        </select>
-                    </label></div>
-                    <div><label>
-                        Source des données
-                        <select onChange={handleSourceChange} defaultValue={source}>
-                        {
-                            Object.keys(sources).map(k => {
-                                return <option key={k} value={k}>{sources[k]}</option>
-                            })
-                        }
-                        </select>
-                    </label></div>
-                    <table>
-                    <tbody>
-                    {
-                        cats.map(cat => {
-                            return (
-                                <tr key={cat}>
-                                    <td>
-                                        <label>{show[cat]}<input type="checkbox" checked={show[cat]} onChange={e => handleShowChange(cat, e.target.checked)} />
-                                        {cat}</label>
-                                    </td>
-                                    <td>
-                                    {Object.keys(catMapping).map(catId => {
-                                        if (catMapping[catId].cat === cat) {
-                                            return <div key={catId}><span style={{color:colors(catId)}}>◼</span>&nbsp;{catMapping[catId].name || catMapping[catId].cat}</div>
-                                        }
-                                    })}
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
-                    </tbody>
-                    </table>
-                </div>
-          {benefits.map(b => {
-            let l = b.label
-            let data = apply(source, b, show)
 
-            if (!data.length) {
-                return
+            h4 small {
+                display: block;
             }
+          `}</style>
+          <h1>Statistiques d'impact et d'aide à l'amélioration du produit Mes Aides</h1>
 
-            return (
-                <div key={l} className="cell">
-                    <h3>{l}</h3>
+          <h2>Résultats de sondage 7 jours après les simulations (sur {survey.count} réponses)</h2>
+
+          <h3>Résumé du sondage</h3>
+            { survey.summary.map(v => (
+                <div key={v.key}><span style={{color:surveyColors(v.category)}}>◼</span>&nbsp;<span dangerouslySetInnerHTML={{ __html: surveyLabels[v.key].legend }}></span></div>
+            ))}
+          <div className="chart doubleCell">
+          <ResponsiveBar
+            label={d => `${d.value} (${Math.round(100*d.value / survey.count)}%)` }
+            data={survey.summary}
+            indexBy="category"
+            keys={["value"]}
+            isInteractive={false}
+            margin={{ top: 15, right: 10, bottom: 50, left: 60 }}
+            padding={0.3}
+            colors={({data}) => surveyColors(data.category) }
+            borderColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
+            animate={false}
+        /></div>
+        <h3>Détails des résultats du sondage par prestation <button onClick={() => setShowSurveyDetails(!showSurveyDetails)}>Afficher/Cacher</button></h3>
+        { showSurveyDetails && (<div className="list">
+            { survey.details.data.map(k => (
+                <div className="cell" key={k.id}>
+                    <h4>
+                        {openfiscaVariables[k.id] && openfiscaVariables[k.id].label || k.id}
+                        <small>sur {k.total} réponses</small>
+                    </h4>
                     <div className="chart">
-                        <MyResponsiveBar data={data} />
+                        <ResponsiveBar
+                            axisLeft={{
+                              format: value =>
+                                `${Number(value)} %`,
+                            }}
+                            label={d => d.data.value }
+                            data={k.data}
+                            maxValue={survey.details.maxPercentage}
+                            indexBy="category"
+                            keys={["percentage"]}
+                            isInteractive={false}
+                            margin={{ top: 15, right: 10, bottom: 50, left: 60 }}
+                            padding={0.3}
+                            colors={({data}) => surveyColors(data.category) }
+                            borderColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
+                            animate={false}
+                        />
                     </div>
                 </div>
-            )
-          })}
-          </div>
+            ))}
+        </div>)}
+
+
+                  <h2>Comportements utilisateur sur la page de résultats <button onClick={() => setShowActions(!showActions)}>Afficher/Cacher</button></h2>
+            { showActions && (<div>
+                <p>
+                    Les graphiques suivants représentent les taux de conversion sur la page de présentation de résultats sur le simulateur.
+                    Différents évènements sont capturés pour mieux évaluer l'impact du simulateur sur le non-recours aux dispositifs présentés aux usagers.
+                </p>
+                <div className="list">
+                    <div className="cell">
+                        <div><label>
+                            Période de référence
+                            <select onChange={handlePeriodChange} defaultValue={period}>
+                            {
+                                Object.keys(periods).map(k => {
+                                    return <option key={k} value={k}>{periods[k]}</option>
+                                })
+                            }
+                            </select>
+                        </label></div>
+                        <div><label>
+                            Source des données
+                            <select onChange={handleSourceChange} defaultValue={source}>
+                            {
+                                Object.keys(sources).map(k => {
+                                    return <option key={k} value={k}>{sources[k]}</option>
+                                })
+                            }
+                            </select>
+                        </label></div>
+                        <table>
+                        <tbody>
+                        {
+                            cats.map(cat => {
+                                return (
+                                    <tr key={cat}>
+                                        <td>
+                                            <label>{show[cat]}<input type="checkbox" checked={show[cat]} onChange={e => handleShowChange(cat, e.target.checked)} />
+                                            {cat}</label>
+                                        </td>
+                                        <td>
+                                        {Object.keys(catMapping).map(catId => {
+                                            if (catMapping[catId].cat === cat) {
+                                                return <div key={catId}><span style={{color:actionColors(catId)}}>◼</span>&nbsp;{catMapping[catId].name || catMapping[catId].cat}</div>
+                                            }
+                                        })}
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                        </tbody>
+                        </table>
+                    </div>
+              {benefits.map(b => {
+                let l = b.label
+                let data = apply(source, b, show)
+
+                if (!data.length) {
+                    return
+                }
+
+                return (
+                    <div key={l} className="cell">
+                        <h3>{l}</h3>
+                        <div className="chart">
+                            <ActionResponsiveBar data={data} />
+                        </div>
+                    </div>
+                )
+              })}
+              </div>
+          </div>) }
+
+
         </div>
     );
 }
