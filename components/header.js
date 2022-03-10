5357617da8742efb43892a5d9f6d40f3109252db
Cugniere <16650011+Cugniere@users.noreply.github.com>
2022-03-10 06:19:14 +0100

    update: rend le menu responsive

--- components/header.js
+++ components/header.js
@@ -8,14 +8,20 @@ const menu = [
   { url: "/pages", label: "Statistiques de visite" },
 ]
 
+function closeMenu() {
+  document.getElementById("menu-checkbox").checked = false;
+}
+
 const Header = () => {
   const router = useRouter().asPath.split("?")[0]
   return (
     <header>
+      <input id="menu-checkbox" type="checkbox" autoComplete="off" />
+      <label id="menu-switch" htmlFor="menu-checkbox"></label>
       <nav>
         {menu.map((item) => (
           <Link href={item.url} key={item.url}>
-            <a className={router == item.url ? "active" : ""}>{item.label}</a>
+            <a className={router == item.url ? "active" : ""} onClick={e => closeMenu()}>{item.label}</a>
           </Link>
         ))}
       </nav>
