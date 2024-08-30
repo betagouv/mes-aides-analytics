790834751f1490a1b2204f2139a2a851fd2bde8d
Shamzic <shamery@free.fr>
2024-08-30 10:40:06 +0200

    chore: màj texte stats d'usage

--- pages/index.js
+++ pages/index.js
@@ -82,12 +82,21 @@ class Index extends Component {
           <a
             target="_blank"
             rel="noreferrer"
-            href="https://stats.data.gouv.fr/index.php?idSite=165&module=MultiSites&action=index&date=yesterday&period=month"
+            href="https://stats.data.gouv.fr/index.php?idSite=165&module=MultiSites&action=index&date=2024-07-29&period=month"
           >
             la page suivante
           </a>{" "}
-          et à côté des statistiques d'usage d'autres Startups d'État. Sur
-          Matomo, le simulateur est intitulé « aides-jeunes ».
+          pour les données jusqu'à juillet 2024 (site web « aides-jeunes ») et{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://stats.beta.gouv.fr/index.php?module=MultiSites&action=index&idSite=63&period=month&date=yesterday"
+          >
+            la page suivante
+          </a>{" "}
+          pour les données à partir d'août 2024 (site web «
+          mes-aides.1jeune1solution.beta.gouv.fr »), à côté des statistiques
+          d'usage d'autres Startups d'État.
         </p>
 
         <h2>Observatoire de la qualité des démarches en ligne</h2>
